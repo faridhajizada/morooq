9cbeeaf1107493de2b6fb53e850d14174a1d321f
Farid0558475484 <faridmh@code.edu.az>
2023-05-22 11:25:47 +0400

    feat:header structur

--- src/pages/exam/index.js
+++ src/pages/exam/index.js
@@ -1,18 +1,37 @@
 import React from "react";
 import Link from "next/link";
 import s from "./../../styles/exam.module.scss";
+import { Dropdown } from "react-bootstrap";
 
 function Exam() {
   return (
     <>
-      <div className={s.examHeader }>
+      <div className={s.examHeader}>
         <div className={s.examSection}>
           <h3>Section 1:Reading and Writing</h3>
-          <p>Directions</p>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Directions
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item className={s.dropdownBody}>
+                <p className={s.dropdownBodyText}>
+     
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
         <div className={s.examTime}>
-          <p>0:00</p>
-          <button>Hidden</button>
+          <h2>0:00</h2>
+          <button>Hide</button>
         </div>
         <div className={s.examTool}>
           <p>Annotate</p>

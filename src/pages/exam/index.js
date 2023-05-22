e62ba5ff7046c446de94fc913448dfe8cafbedfa
Farid0558475484 <faridmh@code.edu.az>
2023-05-22 17:53:12 +0400

    feat:add  BodyRidghtQuestion BodyRightAnswer

--- src/pages/exam/index.js
+++ src/pages/exam/index.js
@@ -1,6 +1,8 @@
 import s from "./../../styles/exam.module.scss";
 import { Dropdown } from "react-bootstrap";
 import React, { useState } from "react";
+import { Container, Row } from "react-bootstrap";
+
 
 function Exam() {
   const [width, setWidth] = useState(50); // Добавлено состояние width и функция setWidth
@@ -23,91 +25,118 @@ function Exam() {
 
   return (
     <>
-      <div className={s.examHeader}>
-        <div className={s.examSection}>
-          <h3>Section 1:Reading and Writing</h3>
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Directions
-            </Dropdown.Toggle>
+      <Container fluid>
+        <Row>
+          <div className={s.examHeader}>
+            <div className={s.examSection}>
+              <h3>Section 1:Reading and Writing</h3>
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Directions
+                </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-              <Dropdown.Item className={s.dropdownBody}>
-                <p className={s.dropdownBodyText}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <Dropdown.Menu>
+                  <Dropdown.Item className={s.dropdownBody}>
+                    <p className={s.dropdownBodyText}>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing
+                      elit, sed do eiusmod tempor incididunt ut labore et dolore
+                      magna aliqua. Lorem ipsum dolor sit amet, consectetur
+                      adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua.
+                    </p>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+            <div className={s.examTime}>
+              <h2>0:00</h2>
+              <button>Hide</button>
+            </div>
+            <div className={s.examTool}>
+              <p>Annotate</p>
+              <p>More</p>
+            </div>
+          </div>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          <div className={s.examBody}>
+            <div className={s.examBodyLeft} style={{ width: `${width}%` }}>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <button onClick={() => handleClick("examBodyLeft")}>Left</button>
+            </div>
+            <div
+              className={s.examBodyRight}
+              style={{ width: `${100 - width}%` }}
+            >
+              <div className={s.BodyRightContent}>
+                <div className={s.count} md={1}>
+                  <p>1</p>
+                </div>
+                <div className={s.markReview} md={1}>
+                  <p>Mark for Review</p>
+                </div>
+                <div className={s.abc} md={10}>
+                  <p>ABC</p>
+                </div>
+              </div>
+              <div className={s.BodyRidghtQuestion}>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed?
                 </p>
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-        <div className={s.examTime}>
-          <h2>0:00</h2>
-          <button>Hide</button>
-        </div>
-        <div className={s.examTool}>
-          <p>Annotate</p>
-          <p>More</p>
-        </div>
-      </div>
+              </div>
+              <div className={s.BodyRightAnswer}>
+                <div className={s.answerBody}>
+                  <p className={s.answerVariant}>A</p>
+                  <p>BMW</p>
+                </div>
+                <div className={s.answerBody}>
+                  <p className={s.answerVariant}>B</p>
+                  <p>Mercedes</p>
+                </div>
+                <div className={s.answerBody}>
+                  <p className={s.answerVariant}>C</p>
+                  <p>Ferrari</p>
+                </div>
+                <div className={s.answerBody}>
+                  <p className={s.answerVariant}>D</p>
+                  <p>Porshe</p>
+                </div>
+              </div>
+              <button onClick={() => handleClick("examBodyRight")}>
+                Right
+              </button>
+            </div>
+          </div>
+        </Row>
+      </Container>
 
-      <div className={s.examBody}>
-        <div className={s.examBodyLeft} style={{ width: `${width}%` }}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua.
-          </p>
-          <button  onClick={() => handleClick("examBodyLeft")}>
-            Left 
-          </button>
-        </div>
-        <div className={s.examBodyBorder}>
-          <div className={s.examBodyBorderTop}></div>
-
-        </div>
-
-        <div className={s.examBodyRight} style={{ width: `${100 - width}%` }}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          
-          </p>
-          <button onClick={() => handleClick("examBodyRight")}>
-            Right 
-          </button>
-        </div>
-      </div>
-
-      <div className={s.examFooter}>
-        <div className={s.examFooterLeft}>
-          <p> Sol Lee</p>
-        </div>
-        <div className={s.examFooterCenter}>
-          <button>Question 1 of 8</button>
-        </div>
-        <div className={s.examFooterRight}>
-          <button>Next</button>
-        </div>
-      </div>
+      <Container>
+        <Row>
+          <div className={s.examFooter}>
+            <div className={s.examFooterLeft}>
+              <p> Sol Lee</p>
+            </div>
+            <div className={s.examFooterCenter}>
+              <button>Question 1 of 8</button>
+            </div>
+            <div className={s.examFooterRight}>
+              <button>Next</button>
+            </div>
+          </div>
+        </Row>
+      </Container>
     </>
   );
 }

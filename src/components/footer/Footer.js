ef004c0d8450643f9dea35aa7f3ef8dfc3bae253
Farid0558475484 <faridmh@code.edu.az>
2023-06-06 10:19:16 +0400

    feat:footer end

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,38 @@
 import React from "react";
+import s from "./../../././styles/footer.module.scss";
+import { Container, Row, Col } from "react-bootstrap";
+import Image from "next/image";
 
 function Footer() {
   return (
     <footer>
-      <div>Footer</div>
+      <Container fluid className={s.footer}>
+        <Row>
+          <div className={s.footerMain}>
+            <div className={s.footerLeft}>
+              <Image
+                className={s.logo}
+                src={"/image/logo.svg"}
+                alt="Picture of the author"
+                width={100}
+                height={100}
+              />
+              <p>morooq, Inc.</p>
+              <p>2035 Sunset Lake Road Suite B-2</p>
+              <p>Newark, DE 19702</p>
+              <p>2018-2023. All Rights Reserved.</p>
+            </div>
+
+            <div className={s.footerLeft}>
+              <h6>SUPPORT</h6>
+              <p>Frequently Ask Questions</p>
+              <p>Terms and Conditions</p>
+              <p>Privacy Policy</p>
+              <p>Contact Us</p>
+            </div>
+          </div>
+        </Row>
+      </Container>
     </footer>
   );
 }

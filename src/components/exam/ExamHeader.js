1a704f7861fc759dec1dff2b128502230ddc71d4
Farid0558475484 <faridmh@code.edu.az>
2023-06-08 15:40:26 +0400

    feat:add exam time

--- src/components/exam/ExamHeader.js
+++ src/components/exam/ExamHeader.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import s from "./../../styles/ExamHeader.module.scss";
 import DirectionsDropdown from "./../../components/dropdown/DirectionsDropdown";
+import { useRouter } from "next/router";
 
 function ExamHeader() {
+  const [time, setTime] = useState(1000); // Изначальное время в секундах (30 минут)
+  const router = useRouter();
+
+  useEffect(() => {
+    if (time > 0) {
+      const timer = setInterval(() => {
+        setTime((prevTime) => prevTime - 1); // Уменьшаем время на 1 секунду
+      }, 1000);
+
+      // Очищаем интервал при размонтировании компонента или при достижении 0 секунд
+      return () => {
+        clearInterval(timer);
+      };
+    } else {
+      // Redirect to "exam-status" page when time runs out
+      router.push("/exam-status");
+    }
+  }, [time, router]);
+
+  // Функция для форматирования времени в формате "минуты:секунды"
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
   return (
     <Container fluid>
       <Row>
         <div className={s.examHeader}>
           <Col md={4}>
             <div className={s.examSection}>
-              {/* <h3> Writing</h3> */}
               <div className={s.dropdown}>
                 <DirectionsDropdown />
               </div>
@@ -18,7 +44,7 @@ function ExamHeader() {
           </Col>
           <Col md={4}>
             <div className={s.examTime}>
-              <h2>0:00</h2>
+              <h2>{formatTime(time)}</h2>
               <button>Hide</button>
             </div>
           </Col>
@@ -28,21 +54,6 @@ function ExamHeader() {
               <p>More</p>
             </div>
           </Col>
-
-          {/* <div className={s.examSection}>
-            <h3>Section 1:Reading and Writing</h3>
-            <div className={s.dropdown}>
-              <DirectionsDropdown />
-            </div>
-          </div>
-          <div className={s.examTime}>
-            <h2>0:00</h2>
-            <button>Hide</button>
-          </div>
-          <div className={s.examTool}>
-            <p>Annotate</p>
-            <p>More</p>
-          </div> */}
         </div>
       </Row>
     </Container>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import "@/styles/globals.scss";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
+
+  
   return (
     <Layout>
       <Component {...pageProps} />

297bc948d9da8953b9ffad21a20ee12b7181a2a0
Farid0558475484 <faridmh@code.edu.az>
2023-06-19 17:18:29 +0400

    feat:change header components

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,34 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Container, Row } from "react-bootstrap";
+
+import styles from "./../../styles/header.module.scss";
 
 function Header() {
   return (
     <header>
-      <div>Header</div>
+      <Container fluid>
+        <Row>
+          <div className={styles.header}>
+            <div className={styles.headerLogo}>
+              <Link href="/">
+                <Image
+                  src="./image/logo.svg"
+                  alt="Logo"
+                  width={150}
+                  height={70}
+                />
+              </Link>
+            </div>
+            <div className={styles.headerSignIn}>
+              <h2 className={styles.userName}>Hacizade Farid</h2>
+              <i className="fa-solid fa-user"></i>
+            </div>
+          </div>
+        </Row>
+      </Container>
     </header>
   );
 }
--- src/components/welcome/WelcomeHeader.js
+++ src/components/welcome/WelcomeHeader.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./../../styles/welcome.module.scss";
-import Image from "next/image";
-import Link from "next/link";
+
 import { Container, Row } from "react-bootstrap";
 
 function WelcomeHeader() {
@@ -10,33 +9,11 @@ function WelcomeHeader() {
       <Container fluid>
         <Row>
           <div className={styles.header}>
-            <div className={styles.container}>
-              <div className={styles.row}>
-                <div className={styles.headerTop}>
-                  <div className={styles.headerLogo}>
-                    <Image
-                      src="./image/logo.svg"
-                      alt="Logo"
-                      width={150}
-                      height={150}
-                    />
-                  </div>
-                  <div className={styles.headerSignIn}>
-                    <h2 className={styles.userName}>Hacizade Farid</h2>
-                    <i className="fa-solid fa-user"></i>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <div className={styles.headerText}>
-                  <p>
-                    Welcome,Farid! Take a practice test and get ready for test
-                    day.
-                  </p>
-                </div>
-              </div>
+            <div className={styles.headerText}>
+              <p>
+                Welcome,Farid! Take a practice test and get ready for test day.
+              </p>
             </div>
-         
           </div>
         </Row>
       </Container>
